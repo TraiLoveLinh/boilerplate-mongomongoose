3f5b1018c3375fa2c9c99733ff449128764716f0
TraiLoveLinh <traipupgmobile@gmail.com>
2023-07-20 07:55:55 +0700

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 //require('dotenv').config();
+
+const mongoose = require("mongoose"); 
 const mongodb_URI = process.env.MONGO_URI;
-const mongoose = require("mongoose");
 
 mongoose.connect(mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
